4afcd4caa40a17a82746cb46b11f0df6fedf5454
Kshitij <kshitijg9899@gmail.com>
2020-06-24 14:51:18 +0530

    validations

--- PHP/signup.php
+++ PHP/signup.php
@@ -26,7 +26,7 @@
 						<h1>SIGN UP</h1>
 					</div>
 					<div class="form-group">
-						<input type="text" class="form-control" name="name" placeholder="Name" pattern="[A-Za-z]{3,100}" required>
+						<input type="text" class="form-control" name="name" placeholder="Name" minlength="3" pattern="[A-Za-z\s]+" required>
 					</div>
 					<div class="form-group">
 						<input type="text" class="form-control" name="email" placeholder="Email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$" required>
@@ -35,7 +35,7 @@
 						<input type="password" class="form-control" name="password" placeholder="Password" pattern=".{8,}" required>
 					</div>
 					<div class="form-group">
-						<input type="tel" class="form-control" name="contact" placeholder="Contact" pattern="[789][0-9]{9}" required>
+						<input type="text" class="form-control" name="contact" placeholder="Contact" pattern="[789][0-9]{9}" required>
 					</div>
 					<div class="form-group">
 						<input type="text" class="form-control" name="city" placeholder="City" required>
--- PHP/signup_script.php
+++ PHP/signup_script.php
@@ -6,25 +6,31 @@
 	$contact = $_POST['contact'];
 	$city = $_POST['city'];
 	$address = $_POST['address'];
-	$name = mysqli_real_escape_string($con, $name);
-	$email = mysqli_real_escape_string($con, $email);
-	$pwd = mysqli_real_escape_string($con, $pwd);
-	$contact = mysqli_real_escape_string($con, $contact);
-	$city = mysqli_real_escape_string($con, $city);
-	$address = mysqli_real_escape_string($con, $address);
-	$pwd = md5($pwd);
-	$sel_query = "SELECT id FROM USERS WHERE email = '" . $email . "'";
-	$sel_query_result = mysqli_query($con, $sel_query) or die (mysqli_error($con));
-	if(mysqli_num_rows($sel_query_result) > 0) {
-		echo "<script>alert('Email already exists.');</script>";
+	if(!(preg_match('`[A-Z]`', $pwd) && preg_match('`[a-z]`', $pwd) && 
+			preg_match('`[0-9]`', $pwd) && preg_match('`[^0-9a-zA-Z]`',$pwd))) {
+		echo "<script>alert('Password must have atleast 1 uppercase, 1 lowercase character, 1 number and 1 special character.');</script>";
 		echo "<script>setTimeout(\"location.href = 'signup.php';\", 1);</script>";
 	} else {
-		$ins_query = "INSERT INTO USERS (name, email, password, contact, city, address) 
-						VALUES ('$name', '$email', '$pwd', '$contact', '$city', '$address')";
-		mysqli_query($con, $ins_query) or die (mysqli_error($con));
-		$user_id = mysqli_insert_id($con);
-		$_SESSION['email'] = $email;
-		$_SESSION['user_id'] = $user_id;
-		header('location: products.php');
+		$name = mysqli_real_escape_string($con, $name);
+		$email = mysqli_real_escape_string($con, $email);
+		$pwd = mysqli_real_escape_string($con, $pwd);
+		$contact = mysqli_real_escape_string($con, $contact);
+		$city = mysqli_real_escape_string($con, $city);
+		$address = mysqli_real_escape_string($con, $address);
+		$pwd = md5($pwd);
+		$sel_query = "SELECT id FROM USERS WHERE email = '" . $email . "'";
+		$sel_query_result = mysqli_query($con, $sel_query) or die (mysqli_error($con));
+		if(mysqli_num_rows($sel_query_result) > 0) {
+			echo "<script>alert('Email already exists.');</script>";
+			echo "<script>setTimeout(\"location.href = 'signup.php';\", 1);</script>";
+		} else {
+			$ins_query = "INSERT INTO USERS (name, email, password, contact, city, address) 
+							VALUES ('$name', '$email', '$pwd', '$contact', '$city', '$address')";
+			mysqli_query($con, $ins_query) or die (mysqli_error($con));
+			$user_id = mysqli_insert_id($con);
+			$_SESSION['email'] = $email;
+			$_SESSION['user_id'] = $user_id;
+			header('location: products.php');
+		}
 	}
 ?>
