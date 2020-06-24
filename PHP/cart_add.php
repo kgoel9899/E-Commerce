395e60d11780d201d6024b1f532e19b55bf099f5
Kshitij <kshitijg9899@gmail.com>
2020-06-24 12:09:27 +0530

    cart

--- PHP/cart_add.php
+++
@@ -1,8 +0,0 @@
-<?php
-	require "../includes/common.php";
-	$item_id = $_GET['id'];
-	$user_id = $_SESSION['user_id'];
-	$ins_query = "INSERT INTO users_items (user_id, item_id, status) VALUES ('$user_id', '$item_id', 'Added to cart')";
-	mysqli_query($con, $ins_query) or die(mysqli_error($con));
-	header("location: products.php")
-?>
